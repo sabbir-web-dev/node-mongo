eeeadf083a9e962546e0bf229307d0c9ff21a5af
sabbir-web-dev <tssabbirhosen@gmail.comf>
2024-01-19 23:34:54 +0600

    creat hard api

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express();
 const port = 3000;
+const user = ["sabbir","tamim","sani","nazim","aslam","mahfuj"];
 
 app.get("/",(req, res) => {
-  res.send('This App Is Call')
+  res.send('This App Is Call');
+})
+
+app.get("/items/book",(req,res) => {
+  res.send({bookName:"JavaScript", author:"sabbir",})
 })
 
 app.listen(port,() =>{
-  console.log(`listening port: ${port}`)
+  console.log(`listening port: ${port}`);
 })
