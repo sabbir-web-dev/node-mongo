638e477ec30c2ca12658e409dbe0925cfcabb227
sabbir-web-dev <tssabbirhosen@gmail.com>
2024-01-19 23:58:35 +0600

    get dinamic

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const port = 3000;
-const user = ["sabbir","tamim","sani","nazim","aslam","mahfuj"];
+const user = ["sabbir","tamim","sani","nazim","aslam","mahfuj","Sagor","Jikirul","siam","alamin"];
 
 app.get("/",(req, res) => {
   res.send('This App Is Call');
@@ -9,6 +9,11 @@ app.get("/",(req, res) => {
 
 app.get("/items/book",(req,res) => {
   res.send({bookName:"JavaScript", author:"sabbir",})
+})
+
+app.get("/user/:id",(req,res)=>{
+  const userId = req.params.id;
+  res.send(user[userId]);
 })
 
 app.listen(port,() =>{
