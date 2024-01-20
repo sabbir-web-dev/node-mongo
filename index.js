113343342bd19cf10f1506d083e3f3050b15c94d
sabbir-web-dev <tssabbirhosen@gmail.com>
2024-01-20 23:47:41 +0600

    add index html

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const user = ["sabbir","tamim","sani","nazim","aslam","mahfuj","Sagor","Jikirul","siam","alamin"];
+
+
+app.use(cors())
 
 app.get("/",(req, res) => {
   res.send('This App Is Call');
@@ -11,9 +15,10 @@ app.get("/items/book",(req,res) => {
   res.send({bookName:"JavaScript", author:"sabbir",})
 })
 
-app.get("/user/:id",(req,res)=>{
-  const userId = req.params.id;
-  res.send(user[userId]);
+app.get("/user/:id",cors(),(req,res)=>{
+  const id = req.params.id;
+  const name = user[id]
+  res.send({id,name})
 })
 
 app.listen(port,() =>{
